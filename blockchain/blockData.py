324e5885b10d3269bc85de58a4288b1424483006
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2021-05-16 17:07:02 -0400

    Added data to BlockData class

--- blockchain/blockData.py
+++ blockchain/blockData.py
@@ -1,13 +1,15 @@
 
 # block data will be dependent on how we design the game
 class BlockData:
-    def __init__(self, id, name):
-        # `id` will depend on game (change later)
-        # `name` will depend on game (change later)
-        self.id = id
-        self.name = name
+    def __init__(self, transactionType, success, mineInt, catchInt, encounter, numGraphicCards, numDogecoin, numEthereum, numBitcoin):
+        # `transactionType` transaction type (str types: mine, validate, sell)
+        # `success` (bool True or False)
+        # `mineInt`, `catchInt`, `encounter`, `numGraphicCards`, `numDogecoin`, `numEthereum`, `numBitcoin` global variables
+        self.transactionType = transactionType
+        self.success = success
+        self.mineInt, self.catchInt, self.encounter, self.numGraphicCards, self.numDogecoin, self.numEthereum, self.numBitcoin = mineInt, catchInt, encounter, numGraphicCards, numDogecoin, numEthereum, numBitcoin
 
     def getTuple(self):
         # tuple should consist of the data that will affect the hash
-        tup = (self.id, self.name)
+        tup = (self.transactionType, self.success, self.mineInt, self.catchInt, self.encounter, self.numGraphicCards, self.numDogecoin, self.numEthereum, self.numBitcoin)
         return tup
