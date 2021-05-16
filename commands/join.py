992d9052c477c93a528fd6b8364d574bb3d624c5
Christophler <chris.lpher15@gmail.com>
2021-05-16 18:37:20 -0400

    Update join.py

--- commands/join.py
+++ commands/join.py
@@ -2,4 +2,4 @@
 async def join(bot):
   print('We have logged in as {0.user}'.format(bot))
   await bot.wait_until_ready()
-  await bot.get_channel(837874716617998370).send("Hello! Get started with the **$help** command! :smiley:") # THE LONG NUMBER IS MY TEST SERVER TEXT CHANNEL ID, SO CHANGE IT WITH YOUR TEXT CHANNEL ID
+  await bot.get_channel(843616163010314260).send("Hello! Get started with the **$help** command! :smiley:") # THE LONG NUMBER IS MY TEST SERVER TEXT CHANNEL ID, SO CHANGE IT WITH YOUR TEXT CHANNEL ID
