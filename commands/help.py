90093ef0b0394e5e032d1643da22f9df2e7b0744
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2021-05-16 17:05:34 -0400

    Small grammar changes to help

--- commands/help.py
+++ commands/help.py
@@ -2,12 +2,12 @@
 async def help(message, discord):
     # Displaying the tutorial
     await message.channel.send('__**TUTORIAL**__')
-    await message.channel.send('Welcome to Cryptomon, a text-based game where you use graphic cards in order to catch cryptocurrency. \n', file=discord.File('images/welcomeCrypto.jpg')) # THIS REQUIRES THE 'welcomeCrypto.jpg'
+    await message.channel.send('Welcome to Cryptomon, a text-based game where you use graphic cards to catch cryptocurrency. \n', file=discord.File('images/welcomeCrypto.jpg')) # THIS REQUIRES THE 'welcomeCrypto.jpg'
 
     # Displaying the command list
     await message.channel.send('__**COMMANDS**__')
     await message.channel.send('**$wallet**: Displays the amount of each crypto and graphic cards you have in your inventory.')
     await message.channel.send('**$stocks**: Displays the stats of each crypto (spawn rate, catch rate, and sell rate).')
     await message.channel.send('**$mine**: Mines a crypto that you can catch.')
-    await message.channel.send('**$validate**: Attempts to catch the crypto.')
+    await message.channel.send('**$validate**: Attempts to catch the crypto you mined.')
     await message.channel.send('**$sell (crypto name) (amount)**: Sells the cryptocurrency and you get graphic cards in return.')
